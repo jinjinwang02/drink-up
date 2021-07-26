c432351975ed1fdd8b80e704e0e55f126e1df48a
Jinjin Wang <jinjinwang02@gmail.com>
2021-07-26 13:51:53 +0100

    feat: add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,21 @@ const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
       />
       <link href="/fonts/font-head.css" rel="stylesheet" />
       <Favicon />
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', '${process.env.GA_TRACKING_ID}');
+        `,
+        }}
+      />
     </Head>
 
     <GlobalStyle />
