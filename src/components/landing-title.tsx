72a304b90da62599d9a10ea839d59e0af17e2c4c
Jinjin Wang <jinjinwang02@gmail.com>
2021-07-26 11:18:53 +0100

    fix: stop landing title animation from retriggering when state changes

--- src/components/landing-title.tsx
+++ src/components/landing-title.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpring } from 'react-spring';
 import { useMediaQuery } from '../hooks/useMediaQuery';
 import { AnimatedBox } from './box/animatedBox';
@@ -9,12 +9,15 @@ import { Typography } from './typography';
 
 const LandingTitle: React.FC = () => {
   const { isXS, isLG } = useMediaQuery();
+  const [animationFinished, setAnimationFinished] = useState<boolean>(false);
+
   const titleFadeInProps = useSpring({
     from: { opacity: 0, y: 40, x: isLG ? '320px' : '0' },
     to: [
       { opacity: 1, y: 0, x: isLG ? '320px' : '0' },
       { opacity: 1, y: 0, x: '0' },
     ],
+    onRest: () => setAnimationFinished(true),
   });
   const footerFadeInProps = useSpring({
     from: { opacity: 0, y: 30, x: isLG ? '210px' : '0' },
@@ -23,14 +26,19 @@ const LandingTitle: React.FC = () => {
       { opacity: 1, y: 0, x: '0' },
     ],
     delay: isLG ? 0 : 300,
+    onRest: () => setAnimationFinished(true),
   });
+
   return (
     <Box
       flexDirection="column"
       alignItems={['flex-start', 'center', 'center', 'flex-start']}
       pb={['zero', 'eight', 'eight', 'zero']}
     >
-      <AnimatedBox style={titleFadeInProps} pb="zeroPointFour">
+      <AnimatedBox
+        style={animationFinished ? undefined : titleFadeInProps}
+        pb="zeroPointFour"
+      >
         <Typography
           textStyle={['h2Brand', 'h2Brand', 'h1Brand']}
           mr={['two', 'three']}
@@ -41,7 +49,7 @@ const LandingTitle: React.FC = () => {
           <Logo size={isXS ? 'small' : 'medium'} />
         </Box>
       </AnimatedBox>
-      <AnimatedBox style={footerFadeInProps}>
+      <AnimatedBox style={animationFinished ? undefined : footerFadeInProps}>
         {isLG ? (
           <TitleWithUnderline variant="secondary">
             A water reminder for your plants.
